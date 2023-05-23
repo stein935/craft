cd198d08d77f77e8f189cb9e35099e7c8d8f7161
Kirk Steineck <stein935@gmail.com>
2023-05-23 10:40:31 -0500

    uninstall

--- config/uninstall.sh
+++ config/uninstall.sh
@@ -41,6 +41,66 @@ warn() {
   printf "${tty_red}Warning${tty_reset}: %s\n" "$(chomp "$1")" >&2
 }
 
+have_sudo_access() {
+  if [[ ! -x "/usr/bin/sudo" ]]
+  then
+    return 1
+  fi
+
+  local -a SUDO=("/usr/bin/sudo")
+  if [[ -n "${SUDO_ASKPASS-}" ]]
+  then
+    SUDO+=("-A")
+  elif [[ -n "${NONINTERACTIVE-}" ]]
+  then
+    SUDO+=("-n")
+  fi
+
+  if [[ -z "${HAVE_SUDO_ACCESS-}" ]]
+  then
+    if [[ -n "${NONINTERACTIVE-}" ]]
+    then
+      "${SUDO[@]}" -l mkdir &>/dev/null
+    else
+      "${SUDO[@]}" -v && "${SUDO[@]}" -l mkdir &>/dev/null
+    fi
+    HAVE_SUDO_ACCESS="$?"
+  fi
+
+  if [[ -n "${CRAFT_ON_MACOS-}" ]] && [[ "${HAVE_SUDO_ACCESS}" -ne 0 ]]
+  then
+    abort "Need sudo access on macOS (e.g. the user ${USER} needs to be an Administrator)!"
+  fi
+
+  return "${HAVE_SUDO_ACCESS}"
+}
+
+execute() {
+  if ! "$@"
+  then
+    abort "$(printf "Failed during: %s" "$(shell_join "$@")")"
+  fi
+}
+
+execute_sudo() {
+  local -a args=("$@")
+  if have_sudo_access
+  then
+    if [[ -n "${SUDO_ASKPASS-}" ]]
+    then
+      args=("-A" "${args[@]}")
+    fi
+    ohai "/usr/bin/sudo" "${args[@]}"
+    execute "/usr/bin/sudo" "${args[@]}"
+  else
+    ohai "${args[@]}"
+    execute "${args[@]}"
+  fi
+}
+
+
+have_sudo_access
+
 while true; do
 
   ohai "Uninstall Craft CLI?"
@@ -96,11 +156,11 @@ if [ "$uninstall" == true ]; then
   servers=$(craft -ls)
   for server in $servers
   do
-    craft stop -n $server &> /dev/null
+    execute "craft stop -n $server &> /dev/null"
   done
   ohai "Deleting Craft CLI files"
-  rm -r /usr/local/craft
-  rm -r /usr/local/bin/craft
+  execute_sudo "rm -r /usr/local/craft"
+  execute_sudo "rm -r /usr/local/bin/craft"
   echo "Deleted: /usr/local/craft"
   echo "Deleted: /usr/local/bin/craft"
 fi
@@ -114,7 +174,7 @@ if [ "$delete_servers" == true ]; then
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-      rm -r $HOME/Craft
+      execute_sudo "rm -r $HOME/Craft"
       echo "Deleted: $HOME/Craft"
       messages+="servers deleted"
 
