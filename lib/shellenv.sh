479d378d6df0ee686a1dccda6e37c51f0df4d426
kstein204_comcast <kirk_steineck@comcast.com>
2025-08-22 09:00:12 -0500

    Fixed all self relative paths to be 100% dynamic

--- lib/shellenv.sh
+++ lib/shellenv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 shellenv_command() {
-  if [[ "${PATH}" == *"${CRAFT_PREFIX}/bin"* ]]; then
+  if [[ "${PATH}" == *"${CRAFT_HOME_DIR}/bin"* ]]; then
     return
   fi
-  echo "export PATH=\"${CRAFT_PREFIX}/bin\${PATH+:\$PATH}\";"
+  echo "export PATH=\"${CRAFT_HOME_DIR}/bin\${PATH+:\$PATH}\";"
   exit 0
 }
